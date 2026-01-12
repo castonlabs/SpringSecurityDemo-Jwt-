db06ca33ee9ca3026574739eca2f5286ffbfd503
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-01-13 00:30:28 +0200

    added a model class product class

---
+++ src/main/java/com/zyratechnologies/jwtproject1/Jwtproject1Application.java
@@ -0,0 +1,13 @@
+package com.zyratechnologies.jwtproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwtproject1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/zyratechnologies/jwtproject1/model/Product.java
@@ -0,0 +1,31 @@
+package com.zyratechnologies.jwtproject1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+}
